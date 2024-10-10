0245a4d600b47147e5c305cc8605543ceaf36d60
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-10-10 16:09:43 -0400

    implementation of abstract fields and displayInfo() abstract method

--- src/main/java/ash_a9236/example/User.java
+++ src/main/java/ash_a9236/example/User.java
@@ -1,4 +1,24 @@
 package ash_a9236.example;
+/*
+ Class User:
+         Represents a generic user in the system.
+         Contains attributes:
+             userID: Unique identifier for the user.
+             name: Name of the user.
 
+         Abstract method:
+             displayInfo(): Displays information specific to each user type.
+ */
 public abstract class User {
+    public static int count = 100;
+    private final String userID;
+    private final String name;
+
+
+    protected User(String userID, String name) {
+        this.name = name;
+        this.userID = name.substring(0, 3).toUpperCase() + count++;
+    }
+
+    abstract void displayInfo(String name, String userID);
 }
