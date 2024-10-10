0f0db86032e20fc55a51f9d5eb9ea214d0b87cf8
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-10-10 17:08:10 -0400

    implementation of abstract fields and displayInfo() abstract method

--- src/main/java/ash_a9236/example/Driver.java
+++ src/main/java/ash_a9236/example/Driver.java
@@ -1,4 +1,9 @@
 package ash_a9236.example;
 
 public class Driver extends User {
+
+
+    protected Driver(String userID, String name) {
+        super(userID, name);
+    }
 }
--- src/main/java/ash_a9236/example/Rider.java
+++ src/main/java/ash_a9236/example/Rider.java
@@ -3,4 +3,7 @@ package ash_a9236.example;
 public class Rider extends User {
 
 
+    protected Rider(String userID, String name) {
+        super(userID, name);
+    }
 }
